de390cdb7ece4a848803fe63cba47621e20c8977
Gregorio Massara <greg.massara@gmail.com>
2017-04-06 13:09:36 +0100

    Email Intent to confirm Order added, removed the order display summary from the app dispaly.

--- MainActivity.java
+++ MainActivity.java
@@ -5,12 +5,14 @@
 package net.gregoriomassara.javaapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -28,20 +30,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the + button is clicked.
-     */
-
-    public void increment(View view) {
-        quantity = quantity + 1;
-        displayQuantity(quantity);
-    }
-
-    /**
      * This method is called when the - button is clicked.
      */
 
     public void decrement(View view) {
+        // Toast message to alert we cannoy order less than 1 Coffee!
+        if (quantity <= 1) {
+            Toast toast = Toast.makeText(getApplicationContext(), "You cannot have less than 1 Coffee!", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+
+        }
         quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method is called when the + button is clicked.
+     */
+
+    public void increment(View view) {
+
+        if (quantity >= 10){
+     // Toast message to alert we cannot order more than 10 Coffees!
+            Toast toast = Toast.makeText(getApplicationContext(), "You cannot have more than 10 Coffees!", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
@@ -53,29 +69,50 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void submitOrder(View view) {
+
         CheckBox checkedBox = (CheckBox) findViewById(R.id.add_whipped_cream);
         boolean hasWhippedCream = checkedBox.isChecked();
 
         CheckBox checkedBox2 = (CheckBox) findViewById(R.id.add_chocolate);
         boolean hasChocolate = checkedBox2.isChecked();
 
-        Log.v("MainActivity.java", "Has Whipped Cream!!!!!!!!!!!!!!!!!!!!" + hasWhippedCream);
-        Log.v("MainActivity.java", "Has Chocolate!!!!!!!!!!!!!!!!!!!!" + hasChocolate);
+        //Entry field for the customer name
+        EditText name = (EditText) findViewById(R.id.customer_name);
+        String customerName = name.getText().toString();
 
-        int price = calculatePrice();
-        String summaryMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
-        displayMessage(summaryMessage);
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+
+        String summaryMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, customerName);
+
+       //Creates an email Intent to send an order confirmation
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, customerName + "'s Coffee Order Summary");
+        intent.putExtra(Intent.EXTRA_TEXT, summaryMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * This method calculates the price of the order.
-     *
      *@return the total price of the coffees
      */
 
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+
+        int basePrice = 5;
+
+/// Adds 1£ to the cost of the coffee
+        if (hasWhippedCream) {
+            basePrice +=  1;
+        }
+// Adds 2£ to the cost of the coffee
+        if (hasChocolate) {
+            basePrice += 2;
+        }
+// Return the total price for the coffees ordered plus any extra chocolate or whipped cream added to the order
+        return quantity * basePrice;
     }
 
     /**
@@ -88,22 +125,13 @@ public class MainActivity extends AppCompatActivity {
      * @return text of the summary order!
      */
 
-    private String createOrderSummary (int price, boolean hasWhippedCream, boolean hasChocolate) {
-        String customerName = "Name: Lyla The Labyrinth";
-        String summary = customerName +  "\nAdd Whipped Cream? " + hasWhippedCream;
+    public String createOrderSummary (int price, boolean hasWhippedCream, boolean hasChocolate, String customerName) {
+        String customerName1 = "Name: " + customerName;
+        String summary = customerName1 +  "\nAdd Whipped Cream? " + hasWhippedCream;
         summary += "\nAdd Chocolate? " + hasChocolate;
         summary += "\nPrice: £" + price + "\nThank You!";
         summary += "\nQuantity: " + quantity;
         return summary;
-    }
-
-    /**
-     * This method displays the given Order Summary on the screen.
-     */
-
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
     }
 
     /**
